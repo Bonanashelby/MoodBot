e42abeaa247d25447aac075e1e575b143f80f8e2
bonanashelby <bonanashelby@gmail.com>
2017-06-13 09:45:28 -0700

    set up the workings for the database

--- mood_bot/mood_bot/tests.py
+++ mood_bot/mood_bot/tests.py
@@ -4,62 +4,128 @@ import transaction
 from pyramid import testing
 
 
+import pytest
+from expense_tracker.models import (
+    Moodbot,
+    get_tm_session,
+)
+from mood_bot.models.meta import Moodbot
+
+
+@pytest.fixture(scope="session")
+def configuration(request):
+    """Set up a Configurator instance.
+
+    This Configurator instance sets up a pointer to the location of the
+        database.
+    It also includes the models from your app's model package.
+    Finally it tears everything down, including the in-memory SQLite database.
+
+    This configuration will persist for the entire duration of your PyTest run.
+    """
+    config = testing.setUp(settings={
+        'sqlalchemy.url': 'postgres:///test_moodbot'
+    })
+    config.include("mood_bot.models")
+
+    def teardown():
+        testing.tearDown()
+
+    request.addfinalizer(teardown)
+    return config
+
+
+@pytest.fixture
+def db_session(configuration, request):
+    """Create a session for interacting with the test database.
+
+    This uses the dbsession_factory on the configurator instance to create a
+    new database session. It binds that session to the available engine
+    and returns a new session for every call of the dummy_request object.
+    """
+    SessionFactory = configuration.registry["dbsession_factory"]
+    session = SessionFactory()
+    engine = session.bind
+    Base.metadata.create_all(engine)
+
+    def teardown():
+        session.transaction.rollback()
+        Base.metadata.drop_all(engine)
+
+    request.addfinalizer(teardown)
+    return session
+
+
 def dummy_request(dbsession):
     return testing.DummyRequest(dbsession=dbsession)
 
 
-class BaseTest(unittest.TestCase):
-    def setUp(self):
-        self.config = testing.setUp(settings={
-            'sqlalchemy.url': 'sqlite:///:memory:'
-        })
-        self.config.include('.models')
-        settings = self.config.get_settings()
-
-        from .models import (
-            get_engine,
-            get_session_factory,
-            get_tm_session,
-            )
-
-        self.engine = get_engine(settings)
-        session_factory = get_session_factory(self.engine)
-
-        self.session = get_tm_session(session_factory, transaction.manager)
-
-    def init_database(self):
-        from .models.meta import Base
-        Base.metadata.create_all(self.engine)
-
-    def tearDown(self):
-        from .models.meta import Base
-
-        testing.tearDown()
-        transaction.abort()
-        Base.metadata.drop_all(self.engine)
+def test_model_gets_added(db_session):
+    assert len(db_session.query(Expense).all()) == 0
+    model = Expense(
+        category="Fake Category",
+        description="Some description text",
+        creation_date=datetime.datetime.now(),
+        amount=12345.67
+    )
+    db_session.add(model)
+    assert len(db_session.query(Expense).all()) == 1
 
 
-class TestMyViewSuccessCondition(BaseTest):
+# class BaseTest(unittest.TestCase):
+#     def setUp(self):
+#         self.config = testing.setUp(settings={
+#             'sqlalchemy.url': 'sqlite:///:memory:'
+#         })
+#         self.config.include('.models')
+#         settings = self.config.get_settings()
 
-    def setUp(self):
-        super(TestMyViewSuccessCondition, self).setUp()
-        self.init_database()
+#         from .models import (
+#             get_engine,
+#             get_session_factory,
+#             get_tm_session,
+#         )
 
-        from .models import MyModel
+#         self.engine = get_engine(settings)
+#         session_factory = get_session_factory(self.engine)
 
-        model = MyModel(name='one', value=55)
-        self.session.add(model)
+#         self.session = get_tm_session(session_factory, transaction.manager)
 
-    def test_passing_view(self):
-        from .views.default import my_view
-        info = my_view(dummy_request(self.session))
-        self.assertEqual(info['one'].name, 'one')
-        self.assertEqual(info['project'], 'mood_bot')
+#     def init_database(self):
+#         """."""
+#         from .models.meta import Base
+#         Base.metadata.create_all(self.engine)
+
+#     def tearDown(self):
+#         """."""
+#         from .models.meta import Base
+
+#         testing.tearDown()
+#         transaction.abort()
+#         Base.metadata.drop_all(self.engine)
 
 
-class TestMyViewFailureCondition(BaseTest):
+# class TestMyViewSuccessCondition(BaseTest):
 
-    def test_failing_view(self):
-        from .views.default import my_view
-        info = my_view(dummy_request(self.session))
-        self.assertEqual(info.status_int, 500)
+#     def setUp(self):
+#         super(TestMyViewSuccessCondition, self).setUp()
+#         self.init_database()
+
+#         from .models import MyModel
+
+#         model = MyModel(name='one', value=55)
+#         self.session.add(model)
+
+#     def test_passing_view(self):
+#         from .views.default import my_view
+#         info = my_view(dummy_request(self.session))
+#         self.assertEqual(info['one'].name, 'one')
+#         self.assertEqual(info['project'], 'mood_bot')
+
+
+# class TestMyViewFailureCondition(BaseTest):
+
+#     def test_failing_view(self):
+#         from .views.default import my_view
+#         info = my_view(dummy_request(self.session))
+#         self.assertEqual(info.status_int, 500)
