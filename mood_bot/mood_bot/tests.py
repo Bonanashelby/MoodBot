b6e2c8bd560f823028c5ffa0697f6258c14903bf
bonanashelby <bonanashelby@gmail.com>
2017-06-14 09:48:08 -0700

    testing I forgot to save.

--- mood_bot/mood_bot/tests.py
+++ mood_bot/mood_bot/tests.py
@@ -72,60 +72,17 @@ def test_model_gets_added(db_session):
     assert len(db_session.query(Expense).all()) == 1
 
 
-# class BaseTest(unittest.TestCase):
-#     def setUp(self):
-#         self.config = testing.setUp(settings={
-#             'sqlalchemy.url': 'sqlite:///:memory:'
-#         })
-#         self.config.include('.models')
-#         settings = self.config.get_settings()
-
-#         from .models import (
-#             get_engine,
-#             get_session_factory,
-#             get_tm_session,
-#         )
-
-#         self.engine = get_engine(settings)
-#         session_factory = get_session_factory(self.engine)
-
-#         self.session = get_tm_session(session_factory, transaction.manager)
-
-#     def init_database(self):
-#         """."""
-#         from .models.meta import Base
-#         Base.metadata.create_all(self.engine)
-
-#     def tearDown(self):
-#         """."""
-#         from .models.meta import Base
-
-#         testing.tearDown()
-#         transaction.abort()
-#         Base.metadata.drop_all(self.engine)
+def test_response_200_home_view():
 
 
-# class TestMyViewSuccessCondition(BaseTest):
 
-#     def setUp(self):
-#         super(TestMyViewSuccessCondition, self).setUp()
-#         self.init_database()
-
-#         from .models import MyModel
-
-#         model = MyModel(name='one', value=55)
-#         self.session.add(model)
-
-#     def test_passing_view(self):
-#         from .views.default import my_view
-#         info = my_view(dummy_request(self.session))
-#         self.assertEqual(info['one'].name, 'one')
-#         self.assertEqual(info['project'], 'mood_bot')
+# def test_response_200_about_view():
+#     pass
 
 
-# class TestMyViewFailureCondition(BaseTest):
+# def test_response_200_app_view():
+#     pass
 
-#     def test_failing_view(self):
-#         from .views.default import my_view
-#         info = my_view(dummy_request(self.session))
-#         self.assertEqual(info.status_int, 500)
+
+# def test_response_200_login():
+#     pass
