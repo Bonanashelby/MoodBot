37e2a396691ae0aa2cf11e3c3df43fa5c66c95c3
bonanashelby <bonanashelby@gmail.com>
2017-06-14 12:32:19 -0700

    working on tests with kurt - heading out for lunch

--- mood_bot/mood_bot/__init__.py
+++ mood_bot/mood_bot/__init__.py
@@ -6,8 +6,7 @@ import os
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    if os.environ.get('DATABASE_URL', ''):
-        settings["sqlalchemy.url"] = os.environ["DATABASE_URL"]
+    settings["sqlalchemy.url"] = os.environ["DATABASE_URL"]
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.include('.models')
--- mood_bot/mood_bot/tests.py
+++ mood_bot/mood_bot/tests.py
@@ -2,7 +2,7 @@ import unittest
 import transaction
 
 from pyramid import testing
-
+from pyramid.response import Response
 
 import pytest
 from mood_bot.models import (
@@ -24,9 +24,10 @@ def configuration(request):
     This configuration will persist for the entire duration of your PyTest run.
     """
     config = testing.setUp(settings={
-        'sqlalchemy.url': 'postgres:///test_moodbot'
+        'sqlalchemy.url': 'postgres:///moodybot'
     })
     config.include("mood_bot.models")
+    config.include("mood_bot.routes")
 
     def teardown():
         testing.tearDown()
@@ -56,24 +57,83 @@ def db_session(configuration, request):
     return session
 
 
-def dummy_request(dbsession):
-    return testing.DummyRequest(dbsession=dbsession)
+@pytest.fixture
+def dummy_request(db_session):
+    """Dummy request fixture."""
+    return testing.DummyRequest(dbsession=db_session)
 
 
-def test_model_gets_added(db_session):
-    assert len(db_session.query(Expense).all()) == 0
-    model = Expense(
-        category="Fake Category",
-        description="Some description text",
-        creation_date=datetime.datetime.now(),
-        amount=12345.67
-    )
-    db_session.add(model)
-    assert len(db_session.query(Expense).all()) == 1
+@pytest.fixture(scope="session")
+def testapp(request):
+    """Testapp."""
+    from webtest import TestApp
+    from mood_bot import main
+
+    app = main({}, **{"sqlalchemy.url": "postgres:///moodybot"})
+    testapp = TestApp(app)
+
+    SessionFactory = app.registry["dbsession_factory"]
+    engine = SessionFactory().bind
+    Base.metadata.create_all(bind=engine)
+
+    def tearDown():
+        Base.metadata.drop_all(bind=engine)
+
+    request.addfinalizer(tearDown)
+
+    return testapp
 
 
-def test_response_200_home_view():
+def test_home_view_returns_response():
+    """Home view returns a Response object."""
+    from mood_bot.views.default import home_view
+    request = testing.DummyRequest()
+    response = home_view(request)
+    assert isinstance(response, dict)
 
+
+def test_login_view_returns_response():
+    """Login view returns a Response object."""
+    from mood_bot.views.default import login
+    request = testing.DummyRequest()
+    response = login(request)
+    assert isinstance(response, dict)
+
+
+def test_login_error(dummy_request):
+    """Test error for login."""
+    from mood_bot.views.default import login
+    dummy_request.method = "POST"
+    data_dict = {'username': 'thisismylogin', 'password': 'notmypassword'}
+    dummy_request.POST = data_dict
+    response = login(dummy_request)
+    assert response == {'error': 'Invalid username or password.'}
+
+
+# def test_login_redirects_to_home_view(dummy_request):
+#     """Test that login redirects to the home page after login."""
+#     from mood_bot.views.default import login
+#     dummy_request.method = "POST"
+#     data_dict = {'username': 'kurtykurt', 'password': 'kurtkurt'}
+#     dummy_request.POST = data_dict
+#     response = login(dummy_request)
+#     assert response.status_code == 302 
+
+
+def test_about_view_returns_response():
+    """about view returns a Response object."""
+    from mood_bot.views.default import about_view
+    request = testing.DummyRequest()
+    response = about_view(request)
+    assert isinstance(response, dict)
+
+
+def test_register_view_returns_response():
+    """register view returns a Response object."""
+    from mood_bot.views.default import register
+    request = testing.DummyRequest()
+    response = register(request)
+    assert isinstance(response, dict)
 
 
 # def test_response_200_about_view():
