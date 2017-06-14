8bff2feb3d65cd8beccf43f81373bd69c49ad395
Kurt <kurtrm@gmail.com>
2017-06-13 17:19:41 -0700

    Attempting a heroku push.

--- mood_bot/mood_bot/__init__.py
+++ mood_bot/mood_bot/__init__.py
@@ -5,6 +5,8 @@ from pyramid.config import Configurator
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+    if os.environ.get('DATABASE_URL', ''):
+        settings["sqlalchemy.url"] = os.environ["DATABASE_URL"]
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.include('.models')
